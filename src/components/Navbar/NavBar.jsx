d7cd88e403a2deaab76b69a9007beabf201ef7df
arielnasimba <arielkafe@hotmail.com>
2024-02-29 16:41:51 +0100

    add updated

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -68,7 +68,7 @@ export default function NavBar() {
             <Link 
              to={`/galaxy/home/`}>
             
-              <h2 className=" " >00 HOME</h2>
+              <h2 className=" " >00 HOME fe</h2>
             </Link>
 
 
