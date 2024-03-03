559cf4c838619cd0447912cd1ca0f7630be0e8d2
arielnasimba <arielkafe@hotmail.com>
2024-03-03 22:08:05 +0100

    add home page and navbar responsive desktop m 1024px

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,35 +10,25 @@ import DATA from "../../assets/data/data.json"
 
 export default function Home() {
 
-  // console.log(DATA);
-
-
-    // DATA.crew.map((crewMember) =>{
-    //   console.log(crewMember);
-    // })
-
-    // DATA.map((element, key) => {
-
-    //   console.log(element);
-    //     // return (
-    //     //     <Link 
-    //     //         key={key}
-    //     //         to={`/details/${key}`}
-    //     //         className='cursor-pointer hover:underline hover:font-semibold' 
-    //     //     >
-    //     //         {element.nom}
-    //     //     </Link>
-    //     // ) 
-    // })
-
-
   return (
 
 
     <>
       <div className={`w-[100dvw]   h-[100dvh]   bg-no-repeat bg-cover 
                           
-                          
+                          min-[768px]:max-[1023px]:bg-[center_top_0rem]   min-[768px]:max-[1023px]:bg-no-repeat
+                          min-[768px]:max-[1023px]:w-[100dvw]   min-[768px]:max-[1023px]:h-[100dvh]
+                          min-[768px]:max-[1023px]:bg-home-tablet min-[768px]:max-[1023px]:bg-cover
+                          min-[768px]:max-[1023px]:flex 
+                          min-[768px]:max-[1023px]:flex-col min-[768px]:max-[1023px]:flex-wrap
+
+
+                          min-[1024px]:max-[1439px]:bg-[center_top_0rem]   min-[1024px]:max-[1439px]:bg-no-repeat
+                          min-[1024px]:max-[1439px]:w-[100dvw]   min-[1024px]:max-[1439px]:h-[100dvh]
+                          min-[1024px]:max-[1439px]:bg-home-desktop min-[1024px]:max-[1439px]:bg-[147%,100%]
+                          min-[1024px]:max-[1439px]:flex 
+                          min-[1024px]:max-[1439px]:flex-col min-[1024px]:max-[1439px]:flex-wrap
+
                           min-[1440px]:bg-home-desktop  min-[1440px]:flex 
                           min-[1440px]:flex-col min-[1440px]:flex-wrap
                       `} 
@@ -48,6 +38,10 @@ export default function Home() {
 
         <div className={`down-box  w-[76%] h-[46.5%] 
         
+                          min-[1024px]:max-[1439px]:  min-[1024px]:max-[1439px]:h-[44.5%]  min-[1024px]:max-[1439px]:w-[78%]
+                          min-[1024px]:max-[1439px]:left-[7.4rem] min-[1024px]:max-[1439px]:absolute 
+                          min-[1024px]:max-[1439px]:bottom-[6.4rem] min-[1024px]:max-[1439px]:flex
+                          min-[1024px]:max-[1439px]: min-[1024px]:max-[1439px]:justify-between
         
                           min-[1440px]:self-center min-[1440px]:absolute 
                           min-[1440px]:bottom-[6.4rem] min-[1440px]:flex
@@ -58,6 +52,10 @@ export default function Home() {
               <div className={`left  text-white
 
 
+                                min-[1024px]:max-[1439px]:h-full  min-[1024px]:max-[1439px]:w-[47%]
+                                min-[1024px]:max-[1439px]:flex min-[1024px]:max-[1439px]:flex-col 
+                                min-[1024px]:max-[1439px]:justify-between
+
                                 min-[1440px]:h-full  min-[1440px]:w-[47%]
                                 min-[1440px]:flex min-[1440px]:flex-col justify-between
 
@@ -66,11 +64,16 @@ export default function Home() {
                                 <div className={`top w-full  
                                   
                                                   h-[12%]
-                                                   min-[1440px]:`}>
+                                                  min-[1024px]:max-[1439px]:
+                                                  min-[1024px]:max-[1439px]:h-[10%] min-[1024px]:max-[1439px]:
+
+                                                   min-[1440px]:
+                                                   `}>
 
                                   <h3 className={`
                                   
                                   
+                                  min-[1024px]:max-[1439px]:text-[1.7rem]
                                   min-[1440px]:text-[2.14rem]
                                   `}   
                                   >SO, YOU WANT TO TRAVEL TO</h3>
@@ -80,11 +83,18 @@ export default function Home() {
                                 <div className={`midd w-full 
                                   
                                                   h-[37%]
-                                                   min-[1440px]:`}>
+                                                  min-[1024px]:max-[1439px]:w-full
+                                                  min-[1024px]:max-[1439px]:h-[30%] min-[1024px]:max-[1439px]:
+
+                                                   min-[1440px]:
+                                                   
+                                                `}>
                                     <h1
                                         className={`
                                   
-                                  
+                                        min-[1024px]:max-[1439px]:text-[8.1rem] min-[1024px]:max-[1439px]:leading-[7rem]
+
+
                                         min-[1440px]:text-[11.1rem] 
                                         min-[1440px]:
                                         min-[1440px]:
@@ -97,6 +107,12 @@ export default function Home() {
                                 <div className={`bot w-[90%] 
                                   
                                                   h-[28%]
+
+                                                  min-[1024px]:max-[1439px]:leading-[2rem]
+                                                  min-[1024px]:max-[1439px]:w-[84%]  min-[1024px]:max-[1439px]:h-[40%]  
+                                                  min-[1024px]:max-[1439px]:text-[1rem]
+                                                   
+
                                                    min-[1440px]:text-[1.38rem]
                                                    `}>
                                       <p>Let's face it; if you want to go to space,
@@ -117,16 +133,24 @@ export default function Home() {
 
                               {/* right side start */}
               <div className={`right bg-white
-                                rounded-full flex text-center justify-center 
-                                items-center
+                               
+                                min-[1024px]:max-[1439px]:rounded-full min-[1024px]:max-[1439px]:flex min-[1024px]:max-[1439px]:text-center 
+                                min-[1024px]:max-[1439px]:justify-center 
+                                min-[1024px]:max-[1439px]:items-center
+                                min-[1024px]:max-[1439px]:h-[40%]  min-[1024px]:max-[1439px]:w-[22.5%]
+                                min-[1024px]:max-[1439px]:self-center  min-[1024px]:max-[1439px]:mr-5 min-[1024px]:max-[1439px]:mb-[7rem]
 
-
+                                min-[1440px]:rounded-full min-[1440px]:flex min-[1440px]:text-center min-[1440px]:justify-center 
+                                min-[1440px]:items-center
                                 min-[1440px]:h-[60%]  min-[1440px]:w-[23.5%]
                                 min-[1440px]:self-center  min-[1440px]:mr-5
 
                               `}>
 
                                 <h4 className={`
+
+
+                                                min-[1024px]:max-[1439px]:text-[1.8rem]
                                 
                                                 text-[2rem] font-light text-black
                                 `}>
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -5,21 +5,23 @@ import LOGO from "../../assets/images/shared/logo.svg"
 
 export default function NavBar() {
 
-    // console.log(DATA.crew);
-    // DATA.crew.map( element => {
-
-    //     if (element.name == 'Douglas Hurley') {
-            
-    //         console.log(element);
-    //     }
-    // }) 
-
   return (
 
    
     <>    
 
       <div className={` fixed 
+        
+                                min-[768px]:max-[1023px]:w-full min-[768px]:max-[1023px]:h-[5.3rem] 
+                                min-[768px]:max-[1023px]:
+                                min-[768px]:max-[1023px]: min-[768px]:max-[1023px]:flex 
+                                min-[768px]:max-[1023px]:justify-between
+        
+                                min-[1024px]:max-[1439px]:w-full min-[1024px]:max-[1439px]:h-[5.3rem] 
+                                min-[1024px]:max-[1439px]:
+                                min-[1024px]:max-[1439px]:mt-9 min-[1024px]:max-[1439px]:flex 
+                                min-[1024px]:max-[1439px]:justify-between
+
         
                                 min-[1440px]:w-full min-[1440px]:h-[6.3rem] min-[1440px]:
                                 min-[1440px]:mt-9 min-[1440px]:flex min-[1440px]:justify-between
@@ -28,21 +30,44 @@ export default function NavBar() {
             
             <div className={`logo_area 
             
-                              
+                                
+                                min-[768px]:max-[1023px]:w-[10%] 
+
+
+                                min-[1024px]:max-[1439px]:w-[7%] 
                                 w-[6%] h-full  flex justify-end items-center min-[1440px]: `}>
 
               <img src={LOGO} alt="" srcset="" className={`
-              
+                                                                 min-[768px]:max-[1023px]:w-[62%]
+                                                                 min-[1024px]:max-[1439px]:w-[60%]
+
+
                                                                 w-[55%] `} />
 
             </div>
+
+
             <div className={`ligne_area 
-                                      min-[1440px]:w-[10%] h-full  min-[1440px]:ml-11 flex justify-center items-center min-[1440px]: `
-                            }>
+
+
+
+                                      
+
+                                      min-[1024px]:max-[1439px]:w-[10%] min-[1024px]:max-[1439px]:h-full  min-[1024px]:max-[1439px]:ml-11 min-[1024px]:max-[1439px]:flex 
+                                      min-[1024px]:max-[1439px]:justify-center min-[1024px]:max-[1439px]:items-center min-[1024px]:max-[1439px]:
+                                      
+                                      
+                                      min-[1440px]:w-[10%] h-full  min-[1440px]:ml-11 flex justify-center items-center min-[1440px]: 
+                                                                                                                                               
+                            `}>
 
                 <span className={`absolute    
+                                               
                 
-                
+                                                min-[1024px]:max-[1439px]:w-[18.5rem] min-[1024px]:max-[1439px]:h-[0.11rem]
+                                                min-[1024px]:max-[1439px]: min-[1024px]:max-[1439px]:
+
+
                                                 min-[1440px]:w-[19.5rem] min-[1440px]:h-[0.11rem]
                                                   bg-white min-[1440px]:
                   
@@ -56,6 +81,21 @@ export default function NavBar() {
           <nav className={`    text-white backdrop-blur-[3px]
         
           
+
+        
+          
+                                
+                                
+                                min-[768px]:max-[1023px]:h-full min-[768px]:max-[1023px]:bg-[rgba(255,255,255,0.04)] 
+                                min-[768px]:max-[1023px]:self-end min-[768px]:max-[1023px]:flex min-[768px]:max-[1023px]:
+                                min-[768px]:max-[1023px]:items-center min-[768px]:max-[1023px]:text-[1rem] min-[768px]:max-[1023px]:tracking-wider min-[768px]:max-[1023px]:w-[57.8%]
+
+
+                                min-[1024px]:max-[1439px]:h-full min-[1024px]:max-[1439px]:bg-[rgba(255,255,255,0.04)] 
+                                min-[1024px]:max-[1439px]:self-end min-[1024px]:max-[1439px]:flex min-[1024px]:max-[1439px]:
+                                min-[1024px]:max-[1439px]:items-center min-[1024px]:max-[1439px]:text-[1rem] min-[1024px]:max-[1439px]:tracking-wider min-[1024px]:max-[1439px]:w-[54.8%]
+
+
                                 min-[1440px]:w-[64.8%] min-[1440px]:h-full min-[1440px]:bg-[rgba(255,255,255,0.04)] 
                                 min-[1440px]:self-end min-[1440px]:flex min-[1440px]:
                                 min-[1440px]:items-center min-[1440px]:text-[1.1rem] min-[1440px]:tracking-wider 
@@ -66,55 +106,113 @@ export default function NavBar() {
 
         <ul className={`  
         
+                                              min-[768px]:max-[1023px]: min-[768px]:max-[1023px]:w-[82%] 
+                                              min-[768px]:max-[1023px]:h-1/2 min-[768px]:max-[1023px]:mx-auto min-[768px]:max-[1023px]:flex min-[768px]:max-[1023px]:gap-[2rem]
+                                              min-[768px]:max-[1023px]: min-[768px]:max-[1023px]:items-center
+                                              min-[768px]:max-[1023px]:justify-between   min-[768px]:max-[1023px]:text-center
+
+                                              min-[1024px]:max-[1439px]: min-[1024px]:max-[1439px]:w-[79%] 
+                                              min-[1024px]:max-[1439px]:h-1/2 min-[1024px]:max-[1439px]:mx-auto min-[1024px]:max-[1439px]:flex min-[1024px]:max-[1439px]:gap-[4rem]
+                                              min-[1024px]:max-[1439px]: min-[1024px]:max-[1439px]:items-center
+                                              min-[1024px]:max-[1439px]:            
+                                              
+                                              
                                               min-[1440px]: min-[1440px]:w-[66%] 
                                               min-[1440px]:h-1/2 min-[1440px]:mx-auto min-[1440px]:flex min-[1440px]:gap-[4rem]
                                               min-[1440px]: min-[1440px]:items-center
                                               min-[1440px]:                        
                       `}>
-          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4`}>
+          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4
+          
+                                min-[768px]:max-[1023px]:hover:underline-offset-[2rem]
+
+                                min-[1024px]:max-[1439px]:hover:underline-offset-[2rem]
+          `}>
 
             <Link 
              to={`/galaxy/`}>
             
-              <h2 className=" " >00 HOME</h2>
+              <h2 className={`
+              
+                              min-[768px]:max-[1023px]:text-[0.85rem]
+
+                              min-[1024px]:max-[1439px]:text-[0.8rem]              
+              `}
+              
+                    >00 HOME</h2>
             </Link>
 
 
           </li>
-          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4`}>
+          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4
+          
+                                min-[768px]:max-[1023px]:hover:underline-offset-[2rem]
+
+                                min-[1024px]:max-[1439px]:hover:underline-offset-[2rem]
+          `}>
 
             <Link
              to={`/galaxy/destination/0`}
               
             >
-              <h2>01 DESTINATION</h2>
+              <h2 className={`
+              
+                              min-[768px]:max-[1023px]:text-[0.85rem]
+
+                              min-[1024px]:max-[1439px]:text-[0.8rem]              
+              `}
+              
+              >01 DESTINATION</h2>
             
             </Link>
           </li>
-          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4`}>
+          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4
+          
+                                min-[768px]:max-[1023px]:hover:underline-offset-[2rem]
+
+                                min-[1024px]:max-[1439px]:hover:underline-offset-[2rem]
+          `}>
 
             <Link
               to={`/galaxy/crew/0`}
             >
-            <h2>02 CREW</h2>
+            <h2  className={`
+            
+                            min-[768px]:max-[1023px]:text-[0.85rem]
+
+                            min-[1024px]:max-[1439px]:text-[0.8rem]              
+            `}
+            
+            >02 CREW</h2>
             
             </Link>
           </li>
-          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4`}>
+          <li className={`hover:underline hover:underline-offset-[2.5rem] hover:decoration-4
+          
+                                min-[768px]:max-[1023px]:hover:underline-offset-[2rem]
+
+                                min-[1024px]:max-[1439px]:hover:underline-offset-[2rem]
+          `}>
 
             <Link
               to={`/galaxy/tech/0`}
          
             >
             
-            <h2>03 TECHNOLOGY</h2>
+            <h2 className={`
+            
+                            min-[768px]:max-[1023px]:text-[0.85rem]
+
+                            min-[1024px]:max-[1439px]:text-[0.8rem]              
+`}
+            
+            
+            >03 TECHNOLOGY</h2>
             </Link>
           </li>
         </ul>
-        
 
           </nav>
-
 
       </div>  
       <Outlet/>
